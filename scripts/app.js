f06dc5bd6ad3c76ed417167bf7d88a92a22c8924
ranjith.j <ranjithjacob.work@gmail.com>
2025-12-15 17:54:50 +0000

    Updates html and begins js

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+//console.log("Hello world!");
+
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+
+const playerStart = document.querySelector("#player-start");
+const playerRestart = document.querySelector("#oops");
+const playerRefresh = document.querySelector("#explore");
+
+/*-------------------------------- Functions --------------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
